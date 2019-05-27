8a2225ba62ffc1fff56905de1445bacc8f231abe
Song <>
2019-05-27 16:21:55 +0800

    1.1.3.3

--- QSExtensions/QSExtensions/Extensions/Setting/UITextField+QSSetting.swift
+++ QSExtensions/QSExtensions/Extensions/Setting/UITextField+QSSetting.swift
@@ -8,46 +8,46 @@
 
 import UIKit
 
-extension UITextField {
+public extension UITextField {
     /// 设置文字
-    public func qs_text(_ text: String) -> UITextField {
+    func qs_text(_ text: String) -> UITextField {
         self.text = text
         return self
     }
     
     /// 设置文字颜色
-    public func qs_textColor(_ color: UIColor) -> UITextField {
+    func qs_textColor(_ color: UIColor) -> UITextField {
         textColor = color
         return self
     }
     
     /// 设置文字字体大小
-    public func qs_font(_ font: UIFont) -> UITextField {
+    func qs_font(_ font: UIFont) -> UITextField {
         self.font = font
         return self
     }
     
     /// 设置占位文字
-    public func qs_placeholder(_ placeholder: String) -> UITextField {
+    func qs_placeholder(_ placeholder: String) -> UITextField {
         self.placeholder = placeholder
         return self
     }
     
     /// 设置占位文字颜色
-    public func qs_placeholderColor(_ color: UIColor) -> UITextField {
+    func qs_placeholderColor(_ color: UIColor) -> UITextField {
         // 设置占位文字颜色
         setValue(color, forKeyPath: "placeholderLabel.textColor")
         return self
     }
     
     /// 设置文字对齐方式
-    public func qs_textAlignment(_ alignment: NSTextAlignment) -> UITextField {
+    func qs_textAlignment(_ alignment: NSTextAlignment) -> UITextField {
         textAlignment = alignment
         return self
     }
     
     /// 设置键盘样式
-    public func qs_keyboardType(_ type: UIKeyboardType) -> UITextField {
+    func qs_keyboardType(_ type: UIKeyboardType) -> UITextField {
         keyboardType = type
         return self
     }
