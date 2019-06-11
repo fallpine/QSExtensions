b8a548c587ac6601d11e4d9a03fe440bb5f228e5
Song <791589545@qq.com>
2019-06-11 22:33:45 +0800

    修改Rx中的分类

--- QSExtensions/QSExtensions/Extensions/Rx/ObservableConvertibleType+Rx.swift
+++ QSExtensions/QSExtensions/Extensions/Rx/ObservableConvertibleType+Rx.swift
@@ -9,14 +9,34 @@
 import RxSwift
 
 extension ObservableConvertibleType where Element: Equatable {
-    /// 判断当前值是否与原值相同
+    /// 判断当前值是否与前一个值相同
     ///
-    /// - Returns: （value：当前值，isEqualOriginValue：是否相同）
-    public func qs_isEqualOldValue() -> Observable<(value: Element, isEqual: Bool)> {
+    /// - Returns: （value：当前值，isEqual：是否相同）
+    public func qs_isEqualToBeforeValue() -> Observable<(value: Element, isEqual: Bool)> {
         return self.asObservable()
             .scan(nil) { acum, x -> (origin: Element?, current: Element)? in
                 if let acum = acum {
                     return (origin: acum.current, current: x)
+                } else {
+                    return (origin: x, current: x)
+                }
+            }.map {
+                if let origin = $0!.origin {
+                    return ($0!.current, isEqual: origin == $0!.current)
+                } else {
+                    return ($0!.current, isEqual: false)
+                }
+        }
+    }
+    
+    /// 判断当前值是否与初始值相同
+    ///
+    /// - Returns: （value：当前值，isEqual：是否相同）
+    public func qs_isEqualToOriginValue() -> Observable<(value: Element, isEqual: Bool)> {
+        return self.asObservable()
+            .scan(nil) { acum, x -> (origin: Element?, current: Element)? in
+                if let acum = acum {
+                    return (origin: acum.origin, current: x)
                 } else {
                     return (origin: x, current: x)
                 }
